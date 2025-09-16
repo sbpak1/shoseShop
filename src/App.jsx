336af10f1ae20be52c3b868cb0a03a424a346d9c
sbpak1 <sbpak1@gmail.com>
2025-09-16 12:51:20 +0900

    네비게이션 바 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
+import AppNavBar from './AppNavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Shoes Shop</h1>
+      <AppNavBar />
     </>
   )
 }
