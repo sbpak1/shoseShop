30451fae46b12aa2fd29ea6d07440b6b6686ae77
sbpak1 <sbpak1@gmail.com>
2025-09-17 10:23:58 +0900

    상세페이지와 중첩라우팅 처리완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,60 @@
 import './App.css'
 import AppNavBar from './AppNavBar'
-
+// assets 폴더 내의 이미지 사용법 -> import 해서 사용
 import img_2 from "./assets/images/shoes2.jpg"
-
 import bg_png from './assets/images/bg.png';
 import { Container, Col, Row } from 'react-bootstrap';
-
 import data from './data/data';
 import { useState } from 'react';
+import Product from './Product';
+import { Routes, Route } from "react-router-dom";
+import Detail from './Detail';
+import About from './About';
 function App() {
   // 상품 정보를 갖는 product 스테이트를 만든다.
   const [product, setProduct] = useState(data);
 
   return (
     <>
+      {/* 네비게이션 바 영역 시작 */}
       <AppNavBar />
+      {/* 네비게이션 바 영역 끝 */}
       {/*메인 대문사진 영역 시작 */}
       <div className='main-bg'
         style={{
           backgroundImage: `url('${bg_png}')`
         }}
       />
+      {/* 메인 대문사진 영역 끝 */}
+      {/* Routing 정보를 한꺼번에 모아놓는 장소 */}
+      {/* 스프링에서 사용하는 컨트롤러 클래스 */}
+      <Routes>
+        <Route path='/' element={<div>메인페이지</div>} />
+        <Route path='/detail' element={<Detail />} />
+        <Route path='/cart' element={<div>장바구니페이지</div>} />
+        {/* 중첩라우팅 처리 */}
+        <Route path='/about' element={<About />}>
+          <Route path='member' element={<div>member page</div>}></Route>
+          <Route path='location' element={<div>location page</div>}></Route>
+        </Route>
+        <Route path='*' element={<div>Page Not Found 404 Error</div>}></Route>
+      </Routes>
+      {/* 상품진열영역 시작 */}
       <Container>
         <Row>
-          <Col className="text-center">
-            <img src="https://zzzmini.github.io/images/shoes1.jpg"
-              width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col className="text-center">
-            <img src={img_2}
-              width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col className="text-center">
-            <img src="/images/shoes3.jpg"
-              width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
+          {
+            product.map((shoes, _) => {
+              return (
+                <Col key={shoes.id} className='text-center'>
+                  {/* Product 콤포넌트 자리 */}
+                  <Product shoes={shoes} />
+                </Col>
+              )
+            })
+          }
         </Row>
       </Container>
+      {/* 상품진열영역 끝 */}
     </>
 
 
--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -1,18 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Nav, Navbar } from 'react-bootstrap/';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap/';
 import './AppNavBar.css'
 
+// Route : 페이지(컴포넌트) 이동 처리
+// Routes : Route를 감싸는 용도
+// Link : 실제로 페이지를 보여주는 역할, Link위치에 컴포넌트 뿌려줌
+// userNavigate : 스크립트 영역에서 링크처리를 하는 함수
+import { Link, useNavigate } from "react-router-dom";
+
 function AppNavBar() {
+
+  const navigate = useNavigate();
+
   return (
     <>
       <div className='App'>
         <Navbar bg="dark" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand>Muzinjang</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/detail') }}>Detail</Nav.Link>
+              <Nav.Link>Cart</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/About') }}>About</Nav.Link>
+              <NavDropdown title="Info" id="basic-nav-dropdown">
+                <NavDropdown.Item onClick={() => { navigate('/about/member') }}>Member</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => { navigate('/about/Location') }}>Location</NavDropdown.Item>
+              </NavDropdown>
             </Nav>
           </Container>
         </Navbar>
