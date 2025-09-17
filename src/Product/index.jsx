fa86104f7e3e98cc81f9d28b6ba633a97e5b659d
sbpak1 <sbpak1@gmail.com>
2025-09-17 11:18:56 +0900

    디테일 작업 완료

--- src/Product/index.jsx
+++ src/Product/index.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 function Product({shoes}) {
   const image = `/images/shoes${shoes.id + 1}.jpg`
+  const navigate = useNavigate();
   return (
     <>
-      <img src={image} width="80%" />
+      <img src={image} onClick={()=>{navigate(`/detail/${shoes.id+1}`)}} width="80%" />
       <h4>{shoes.title}</h4>
       <p>{shoes.content}</p>
     </>
