578edfb4ca190d6fa2c0fbdb08f97266438e550b
sbpak1 <sbpak1@gmail.com>
2025-09-18 17:48:33 +0900

    데이터 가져오기 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
 import AppNavBar from './AppNavBar'
-// assets 폴더 내의 이미지 사용법 -> import 해서 사용
-import img_2 from "./assets/images/shoes2.jpg"
-import bg_png from './assets/images/bg.png';
-
 import data from './data/data';
 import { useState } from 'react';
-import Product from './Product';
 import { Routes, Route } from "react-router-dom";
 import Detail from './Detail';
 import About from './About';
 import Home from './Home';
+
 function App() {
   // 상품 정보를 갖는 product 스테이트를 만든다.
   const [product, setProduct] = useState(data);
@@ -20,13 +16,7 @@ function App() {
       {/* 네비게이션 바 영역 시작 */}
       <AppNavBar />
       {/* 네비게이션 바 영역 끝 */}
-      {/*메인 대문사진 영역 시작 */}
-      <div className='main-bg'
-        style={{
-          backgroundImage: `url('${bg_png}')`
-        }}
-      />
-      {/* 메인 대문사진 영역 끝 */}
+     
       {/* Routing 정보를 한꺼번에 모아놓는 장소 */}
       {/* 스프링에서 사용하는 컨트롤러 클래스 */}
       <Routes>
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,8 +1,17 @@
 import { Container, Col, Row, Button } from 'react-bootstrap';
 import Product from '../Product';
 import axios from 'axios';
+import { useState } from 'react';
+// assets 폴더 내의 이미지 사용법 -> import 해서 사용
+import bg_png from '../assets/images/bg.png';
 
 function Home({ product, setProduct }) {
+  // 몇번 눌렀는지 체크하는 스테이트
+  let [dataLoadingCount, setDataLoadingCount] = useState(0)
+
+  // 로딩 중 처리 스테이트
+  let [loadingState, setLoadingState] = useState(true)
+
   return (
     <>
       {/* 상품진열영역 시작 */}
@@ -21,16 +30,37 @@ function Home({ product, setProduct }) {
         </Row>
       </Container>
       {/* 상품진열영역 끝 */}
+      {/* 로딩 메시지 */}
+      <div className='text-center my-3'>
+        {loadingState && <div>Loading ...Please wait</div>}
+        </div>    
+
       <div className='d-flex justify-content-center
         align-items-center'>
         <Button variant="primary" size="lg"
           onClick={async () => {
+            let getUri = ''
+            if(dataLoadingCount == 0){
+              getUri = 'https://zzzmini.github.io/js/react_data_01.json';
+              setDataLoadingCount(dataLoadingCount + 1);
+              setLoadingState(true)
+            } else if(dataLoadingCount == 1){
+              getUri = 'https://zzzmini.github.io/js/react_data_02.json';
+              setDataLoadingCount(dataLoadingCount + 1);
+              setLoadingState(true)
+            } else {
+              alert('데이터가 존재하지 않아요')
+              return;
+            }
+
             try {
-              const result = await axios('https://zzzmini.github.io/js/react_data_01.json')
+              const result = await axios(getUri)
               let temp = [...product, ...result.data];              
               setProduct(temp);
             } catch (error) {
               console.log("가져오기 실패", error)
+            } finally {
+              setLoadingState(false)
             }
 
           // 데이터를 3개 가져오는 함수
