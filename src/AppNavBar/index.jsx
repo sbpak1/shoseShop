0a6859ba20fb9801cf4943ad30bf3e21d004349b
sbpak1 <sbpak1@gmail.com>
2025-09-23 14:54:04 +0900

    firebase 이용한 Login, Logout 처리 / 유저 이미지, 이메일 나타내기 마무리

--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -8,22 +8,50 @@ import './AppNavBar.css'
 import { Link, useNavigate } from "react-router-dom";
 // import {UserContext} from "../context/UserContext.jsx"
 // import { useContext } from "react";
-
 import userStore from "../store/userStore";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { useState } from "react";
 
-function AppNavBar(){
+function AppNavBar() {
   // const {loginUser} = useContext(UserContext);
   // console.log(loginUser)
 
   // store에서 정보 가져오기(zustand 사용)
-  const {userName, productStock, productName, changeName, addProduct} 
-          = userStore();
+  const { userName, productStock, productName, changeName, addProduct }
+    = userStore();
 
   // console.log(productName);
   // console.log(productStock);
+  // 로그인 정보를 담은 스테이트 선언
+  const [userInfo, setUserInfo] = useState(undefined);
 
   const navigate = useNavigate();
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
 
+
+  const handleAuth = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        setUserInfo(user)
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const handleLogOut = () => {
+    signOut(auth)
+    .then(()=> {
+      setUserInfo(undefined)
+      navigate("/");
+    })
+    .catch(error)
+  }
+  
   return (
     <>
       <div className="App">
@@ -31,23 +59,45 @@ function AppNavBar(){
           <Container>
             <Navbar.Brand>Muzinjang</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-              <Nav.Link onClick={()=>{navigate('/cart')}}>Cart</Nav.Link>
-              <Nav.Link onClick={()=>{navigate('/recent')}}>Recent</Nav.Link>
-              <Nav.Link onClick={()=>{navigate('/about')}}>About</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+
+              {userInfo && (
+                <Nav.Link onClick={() => { navigate('/cart') }}>Cart</Nav.Link>
+              )}
+
+              <Nav.Link onClick={() => { navigate('/recent') }}>Recent</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/about') }}>About</Nav.Link>
               <NavDropdown title="Info" id="basic-nav-dropdown">
-                <NavDropdown.Item onClick={()=>{navigate('/about/member')}}>Member</NavDropdown.Item>
-                <NavDropdown.Item onClick={()=>{navigate('/about/location')}}>Location</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => { navigate('/about/member') }}>Member</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => { navigate('/about/location') }}>Location</NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Nav className="ms-auto align-items-center">
-              <Nav.Link as="span" 
+              {
+                userInfo ? (
+                  <div className="d-flex align-item-center userInfoArea">
+                    <img
+                      src={userInfo.photoURL}
+                      alt={userInfo.displayName}
+                      className="userImage"
+                    />
+                    <span className="me-3 text-light small">
+                      {userInfo.email || userInfo.displayName}
+                    </span>
+                    <Nav.Link onClick={handleLogOut}>LogOut</Nav.Link>
+                  </div>
+                ) : (
+                  <Nav.Link onClick={handleAuth}>Login</Nav.Link>
+                )
+              }
+
+              {/* <Nav.Link as="span" 
                   onClick={()=>{
                     changeName()
                     addProduct('르무통', 5)
                   }}  >
                   유저변경</Nav.Link>
-              <Nav.Link as="span">{`${userName}님 로그인 됨.`}</Nav.Link>
+              <Nav.Link as="span">{`${userName}님 로그인 됨.`}</Nav.Link> */}
             </Nav>
           </Container>
         </Navbar>
---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZyrYs2Frmlbxs8p_v6p0m60W2CvmRSV4",
+  authDomain: "react-shop-app-190e2.firebaseapp.com",
+  projectId: "react-shop-app-190e2",
+  storageBucket: "react-shop-app-190e2.firebasestorage.app",
+  messagingSenderId: "157091684976",
+  appId: "1:157091684976:web:2632d62c480b73cc3e4ab5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
